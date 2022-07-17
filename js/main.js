62d97a72170e627879a8809305972a312e159a8b
marcelo.gil <marceloalgil@gmail.com>
2022-07-17 18:02:36 -0300

    agregue funciones y boton + - y cantidad al carrito

--- js/main.js
+++ js/main.js
@@ -17,34 +17,65 @@ const carrito = {}
 
 function sumarProducto(idArt){
     console.log(carrito);
+    const miProducto=buscarProductoSku(idArt);
     if (idArt in carrito){
         carrito[idArt].cantidad = carrito[idArt].cantidad + 1;
-        carrito[idArt].total = productos[idArt].precio*carrito[idArt].cantidad;
+        carrito[idArt].total = miProducto.precio*carrito[idArt].cantidad;
     }else {
-        carrito[idArt] = {sku: productos[idArt].sku, cantidad: 1,total:productos[idArt].precio};
+        carrito[idArt] = {sku: miProducto.sku,nombre: miProducto.nombre, cantidad: 1,total:miProducto.precio};
     }
-    let [totalCarrito, cantidadTotal] = calculaTotal();
+    let [totalCarrito, cantidadTotal,contadorProducto] = calculaTotal();
     // carrito.push({sku: sku, cantidad: 1})
-    ActualizarTotalCarrito(totalCarrito, cantidadTotal);
+    ActualizarTotalCarrito(totalCarrito, cantidadTotal,contadorProducto);
     console.log(cantidadTotal);
     console.log(totalCarrito);
+    console.log(carrito);
+}
+function restarProducto(idArt){
+    console.log(carrito);
+    const miProducto=buscarProductoSku(idArt);
+    if (idArt in carrito){
+        if (carrito[idArt].cantidad != 0){
+            carrito[idArt].cantidad = carrito[idArt].cantidad - 1;
+            carrito[idArt].total = miProducto.precio*carrito[idArt].cantidad;
+        }
+        if (carrito[idArt].cantidad === 0){
+            delete carrito[idArt];
+        }
+        let [totalCarrito, cantidadTotal,contadorProducto] = calculaTotal();
+        ActualizarTotalCarrito(totalCarrito, cantidadTotal,contadorProducto);
+        console.log(cantidadTotal);
+        console.log(totalCarrito);
+        console.log(carrito);
+    }
 }
 function calculaTotal(){
     let total = 0,cantidadTotal = 0;
-
+    let contadorProductos = [0,0,0,0,0,0,0,0,0];
     for (const sku in carrito) {
         total += carrito[sku].total;
         cantidadTotal += carrito[sku].cantidad;
+        contadorProductos[sku] += carrito[sku].cantidad;
     }
-    return [total,cantidadTotal];
+    console.log(contadorProductos);
+
+    return [total,cantidadTotal,contadorProductos];
 }
-function ActualizarTotalCarrito(totalCarrito,cantidadTotal){
+function ActualizarTotalCarrito(totalCarrito,cantidadTotal,contadorProducto){
+    document.getElementById('idContadorProducto1').innerHTML=contadorProducto[1];
+    document.getElementById('idContadorProducto2').innerHTML=contadorProducto[2];
+    document.getElementById('idContadorProducto3').innerHTML=contadorProducto[3];
+    document.getElementById('idContadorProducto4').innerHTML=contadorProducto[4];
+    document.getElementById('idContadorProducto5').innerHTML=contadorProducto[5];
+    document.getElementById('idContadorProducto6').innerHTML=contadorProducto[6];
+    document.getElementById('idContadorProducto7').innerHTML=contadorProducto[7];
+    document.getElementById('idContadorProducto8').innerHTML=contadorProducto[8];
     document.getElementById('idContadorCarrito').innerHTML=cantidadTotal;    
     document.getElementById('idTotalCarrito').innerHTML="Total: "+totalCarrito.toFixed(2)+" $";
     let subTotalCarrito=totalCarrito/(1+alicuota/100);
     let ivaCarrito=subTotalCarrito*(alicuota/100);
     document.getElementById('idIvaCarrito').innerHTML="Iva: "+ivaCarrito.toFixed(2)+" $";
-    document.getElementById('idSubTotalCarrito').innerHTML="Iva: "+subTotalCarrito.toFixed(2)+" $";
+    document.getElementById('idSubTotalCarrito').innerHTML="Subtotal: "+subTotalCarrito.toFixed(2)+" $";
 }
 
 function agregaProducto(producto) {
@@ -78,10 +109,14 @@ function buscarProducto(idnombre) {
     console.log(productosBuscados);
     return productosBuscados;
 }
-
+function buscarProductoSku(idSku) {    
+    const productosBuscados = productos.find(producto => producto.sku===idSku);
+    console.log(productosBuscados);
+    return productosBuscados;
+}
 while (nombreProducto!="ESC" && nombreProducto!="esc"){
-    nombreProducto = prompt("Ingrese nombre del Producto                ESC-Termina ").toUpperCase();
-    if (nombreProducto=="ESC") {
+    nombreProducto = prompt("Ingrese nombre del Producto                ESC-Termina ");
+    if (nombreProducto=="ESC" || nombreProducto=="esc") {
         break;
     }
     
