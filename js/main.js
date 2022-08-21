44c170c05f11481ead2a4be721e97329540f408e
marcelo.gil <marceloalgil@gmail.com>
2022-08-20 23:25:04 -0300

    optimizando

--- js/main.js
+++ js/main.js
@@ -49,113 +49,6 @@ function limpiarProductoshtml(){
     nuevos.innerHTML = "";
 }
 
-function agregaProductohtml({sku: idArt,nombre: nombreProducto, precio: precioProducto, oferta: ofertaProducto, imagenArt, imagenMeli}){
-    let nuevos = document.querySelector("#ProductosNuevos");
-
-    const fragment = document.createDocumentFragment();
-
-    const divOferta = document.createElement('div');
-    divOferta.className ="col mb-5"
-    fragment.appendChild(divOferta);
-    
-    const divOferta0 = document.createElement('div');
-    divOferta0.className = "card h-100";
-    divOferta.appendChild(divOferta0);
-    
-    const divOferta1 = document.createElement('div');
-    divOferta1.id="oferta"+idArt;
-    divOferta1.className = "badge bg-dark text-dark position-absolute" ;
-    divOferta1.style="top: 0.5rem; right: 0.5rem"
-    divOferta1.textContent = (ofertaProducto) ? "Oferta" : "";
-    divOferta0.appendChild(divOferta1);
-    
-    const imagenArticulo = document.createElement('img');
-    imagenArticulo.className = "imagenPequenia";
-    if (imagenMeli){
-        imagenArticulo.className +=(imagenMeli) ? " imagenMercadoLibre":"card-img-top";
-    };
-    imagenArticulo.src = imagenArt;
-    imagenArticulo.alt = "Imagen de Articulo";
-    divOferta0.appendChild(imagenArticulo);
-        
-    const articuloNombre = document.createElement('div');
-    articuloNombre.className = "card-body p-4";
-    divOferta0.appendChild(articuloNombre);
-
-    const articuloNombre1 = document.createElement('div');
-    articuloNombre1.className = "text-center";
-    articuloNombre.appendChild(articuloNombre1);
-
-    const articuloNombre2 = document.createElement('h5');
-    articuloNombre2.className = "fw-bolder";
-    articuloNombre2.textContent = nombreProducto;
-    articuloNombre2.style.color = (ofertaProducto) ? "red" : "black";
-    articuloNombre.appendChild(articuloNombre2);
-
-    const divEstrellas = document.createElement('div');
-    divEstrellas.className = "d-flex justify-content-center small text-warning mb-2"
-    divOferta0.appendChild(divEstrellas);
-
-    const estrellas = document.createElement('div');
-    estrellas.className = "bi-star-fill";
-    divEstrellas.appendChild(estrellas);
-    const estrellas2 = document.createElement('div');
-    estrellas2.className = "bi-star-fill";
-    divEstrellas.appendChild(estrellas2);
-    const estrellas3 = document.createElement('div');
-    estrellas3.className = "bi-star-fill";
-    divEstrellas.appendChild(estrellas3);
-    const estrellas4 = document.createElement('div');
-    estrellas4.className = "bi-star-fill";
-    divEstrellas.appendChild(estrellas4);
-    const estrellas5 = document.createElement('div');
-    estrellas5.className = "bi-star-fill";
-    divEstrellas.appendChild(estrellas5);
-    const articuloPrecio = document.createElement('p');
-    articuloPrecio.className ="text-center";
-    articuloPrecio.textContent = "$"+precioProducto;
-    articuloNombre.appendChild(articuloPrecio);
-    
-    /* botones y contador */
-    const articuloAccion = document.createElement('div');
-    articuloNombre.className = "card-footer p-4 pt-0 border-top-0 bg-transparent";
-    divOferta0.appendChild(articuloAccion);
-
-    const articuloAccion1 = document.createElement('div');
-    articuloAccion1.className = "text-center";
-    articuloAccion.appendChild(articuloAccion1);
-    /* boton + */
-    const articuloAbtn = document.createElement('a');
-    articuloAbtn.id = "idsumaProducto"+idArt;
-    articuloAbtn.className = "btn  mt-auto";
-    articuloAbtn.onclick = () => sumarProducto(idArt); 
-    articuloAccion1.appendChild(articuloAbtn);
-        
-    const articuloAbtnimg = document.createElement('img');
-    articuloAbtnimg.src = "./imagenes/sumar.png";
-    articuloAbtn.appendChild(articuloAbtnimg);
-    
-    /* contador */
-    const articuloContador = document.createElement('span');
-    articuloContador.id = "idContadorProducto"+idArt;
-    articuloContador.className="badge  bg-dark2 text-dark ms-1";
-    articuloContador.textContent = 0;
-    articuloAccion1.appendChild(articuloContador);
-
-    /* boton - */
-    const articuloAbtnResta = document.createElement('a');
-    articuloAbtnResta.id = "idRestaProducto"+idArt;
-    articuloAbtnResta.className = "btn  mt-auto";
-    articuloAbtnResta.onclick = () =>  restarProducto(idArt);
-    articuloAccion1.appendChild(articuloAbtnResta);
-    
-    const articuloAbtnimgResta = document.createElement('img');
-    articuloAbtnimgResta.src = "./imagenes/restar.png";
-    articuloAbtnResta.appendChild(articuloAbtnimgResta);
-    /* inserto en el html */
-    fragment.appendChild(divOferta);
-    nuevos.appendChild(fragment);    
-}
 function sumarProducto(idArt){
     const miProducto=buscarProductoSku(idArt);
     if (idArt in carrito){
@@ -222,12 +115,25 @@ function agregaProducto(producto) {
 function ordenarPrecio() {
     const productosOrdenados = [...productos]
     const resultado=productosOrdenados.sort((a,b) => a.precio - b.precio);
-    mostrarResultado(resultado);
+    const imputId = document.getElementById("fname");
+    if (imputId.value!="") {
+        const productosBuscados = resultado.filter(producto => producto.nombre.includes(imputId.value.toUpperCase()));
+        mostrarResultado(productosBuscados);
+    } else {
+        mostrarResultado(resultado);
+    }
 }
+
 function ordenarNombre() {
     const productosOrdenados = [...productos]
     const resultado=productosOrdenados.sort((a,b) => a.nombre.localeCompare(b.nombre));
-    mostrarResultado(resultado);
+    const imputId = document.getElementById("fname");
+    if (imputId.value!="") {
+        const productosBuscados = resultado.filter(producto => producto.nombre.includes(imputId.value.toUpperCase()));
+        mostrarResultado(productosBuscados);
+    } else {
+        mostrarResultado(resultado);
+    }
 }
 function buscarProducto(idnombre) {    
     const productosBuscados = productos.find(producto => producto.nombre.includes(idnombre));
@@ -248,12 +154,6 @@ function buscarOfertas() {
 function buscarNovedades() { 
     const productosBuscados = productos.filter(producto => producto.novedad);
     return productosBuscados;
-}
-function updateValue(e) {
-    let letras=e.srcElement.value;
-    letras=letras.toUpperCase();
-    const resultado = buscarProductos(letras);
-    mostrarResultado(resultado);
 }
 function updateOfertas() {
     const resultado = buscarOfertas();
@@ -376,6 +276,157 @@ function controlTelefono() {
         return true
     }
 }
+
+function updateValue(e) {
+    let letras=e.srcElement.value;
+    letras=letras.toUpperCase();
+    const resultado = buscarProductos(letras);
+    //mostrarResultado(resultado);
+    const ordenPrecio=document.getElementById("OrdenPrecio")
+    if (ordenPrecio.checked){
+        ordenarPrecio();
+    } else {
+        ordenarNombre();
+    }
+}
+
+function updateValueMerlib(){
+    const imputId = document.getElementById("fname");
+    const texto = imputId.value;
+    if(texto==""){
+        Swal.fire({
+            title: 'Mercado Libre',
+            icon: 'warning',
+            text: 'Falta texto en buscar!!'
+        })
+    } else {
+        limpiarProductoshtml();
+        TraerProductoMercadoLibre(texto).then( () => { 
+            armoElCarrito();
+        })
+    }
+}
+
+function updateValueTablaLocal(){
+    limpiarProductoshtml();
+    traerProductosEnJson().then( () => { 
+        const imputId = document.getElementById("fname");
+        const texto = imputId.value.toUpperCase();
+        const resultado= (texto != "") ? buscarProductos(texto):[];
+        for (const producto of resultado) {
+            agregaProductohtml(producto);
+        }        
+    })
+}
+
+function agregaProductohtml({sku: idArt,nombre: nombreProducto, precio: precioProducto, oferta: ofertaProducto, imagenArt, imagenMeli}){
+    let nuevos = document.querySelector("#ProductosNuevos");
+
+    const fragment = document.createDocumentFragment();
+
+    const divOferta = document.createElement('div');
+    divOferta.className ="col mb-5"
+    fragment.appendChild(divOferta);
+    
+    const divOferta0 = document.createElement('div');
+    divOferta0.className = "card h-100";
+    divOferta.appendChild(divOferta0);
+    
+    const divOferta1 = document.createElement('div');
+    divOferta1.id="oferta"+idArt;
+    divOferta1.className = "badge bg-dark text-dark position-absolute" ;
+    divOferta1.style="top: 0.5rem; right: 0.5rem"
+    divOferta1.textContent = (ofertaProducto) ? "Oferta" : "";
+    divOferta0.appendChild(divOferta1);
+    
+    const imagenArticulo = document.createElement('img');
+    imagenArticulo.className = "imagenPequenia";
+    if (imagenMeli){
+        imagenArticulo.className +=(imagenMeli) ? " imagenMercadoLibre":"card-img-top";
+    };
+    imagenArticulo.src = imagenArt;
+    imagenArticulo.alt = "Imagen de Articulo";
+    divOferta0.appendChild(imagenArticulo);
+        
+    const articuloNombre = document.createElement('div');
+    articuloNombre.className = "card-body p-4";
+    divOferta0.appendChild(articuloNombre);
+
+    const articuloNombre1 = document.createElement('div');
+    articuloNombre1.className = "text-center";
+    articuloNombre.appendChild(articuloNombre1);
+
+    const articuloNombre2 = document.createElement('h5');
+    articuloNombre2.className = "fw-bolder";
+    articuloNombre2.textContent = nombreProducto;
+    articuloNombre2.style.color = (ofertaProducto) ? "red" : "black";
+    articuloNombre.appendChild(articuloNombre2);
+
+    const divEstrellas = document.createElement('div');
+    divEstrellas.className = "d-flex justify-content-center small text-warning mb-2"
+    divOferta0.appendChild(divEstrellas);
+
+    const estrellas = document.createElement('div');
+    estrellas.className = "bi-star-fill";
+    divEstrellas.appendChild(estrellas);
+    const estrellas2 = document.createElement('div');
+    estrellas2.className = "bi-star-fill";
+    divEstrellas.appendChild(estrellas2);
+    const estrellas3 = document.createElement('div');
+    estrellas3.className = "bi-star-fill";
+    divEstrellas.appendChild(estrellas3);
+    const estrellas4 = document.createElement('div');
+    estrellas4.className = "bi-star-fill";
+    divEstrellas.appendChild(estrellas4);
+    const estrellas5 = document.createElement('div');
+    estrellas5.className = "bi-star-fill";
+    divEstrellas.appendChild(estrellas5);
+    const articuloPrecio = document.createElement('p');
+    articuloPrecio.className ="text-center";
+    articuloPrecio.textContent = "$"+precioProducto;
+    articuloNombre.appendChild(articuloPrecio);
+    
+    /* botones y contador */
+    const articuloAccion = document.createElement('div');
+    articuloNombre.className = "card-footer p-4 pt-0 border-top-0 bg-transparent";
+    divOferta0.appendChild(articuloAccion);
+
+    const articuloAccion1 = document.createElement('div');
+    articuloAccion1.className = "text-center";
+    articuloAccion.appendChild(articuloAccion1);
+    /* boton + */
+    const articuloAbtn = document.createElement('a');
+    articuloAbtn.id = "idsumaProducto"+idArt;
+    articuloAbtn.className = "btn  mt-auto";
+    articuloAbtn.onclick = () => sumarProducto(idArt); 
+    articuloAccion1.appendChild(articuloAbtn);
+        
+    const articuloAbtnimg = document.createElement('img');
+    articuloAbtnimg.src = "./imagenes/sumar.png";
+    articuloAbtn.appendChild(articuloAbtnimg);
+    
+    /* contador */
+    const articuloContador = document.createElement('span');
+    articuloContador.id = "idContadorProducto"+idArt;
+    articuloContador.className="badge  bg-dark2 text-dark ms-1";
+    articuloContador.textContent = 0;
+    articuloAccion1.appendChild(articuloContador);
+
+    /* boton - */
+    const articuloAbtnResta = document.createElement('a');
+    articuloAbtnResta.id = "idRestaProducto"+idArt;
+    articuloAbtnResta.className = "btn  mt-auto";
+    articuloAbtnResta.onclick = () =>  restarProducto(idArt);
+    articuloAccion1.appendChild(articuloAbtnResta);
+    
+    const articuloAbtnimgResta = document.createElement('img');
+    articuloAbtnimgResta.src = "./imagenes/restar.png";
+    articuloAbtnResta.appendChild(articuloAbtnimgResta);
+    /* inserto en el html */
+    fragment.appendChild(divOferta);
+    nuevos.appendChild(fragment);    
+}
+
 function displaycarrito() {
     let [totalCarrito, cantidadTotal] = calculaTotal();
     if (totalCarrito!=0){
@@ -478,33 +529,7 @@ function armoElCarrito() {
     });
 }
 
-function updateValueMerlib(){
-    const imputId = document.getElementById("fname");
-    const texto = imputId.value;
-    if(texto==""){
-        Swal.fire({
-            title: 'Mercado Libre',
-            icon: 'warning',
-            text: 'Falta texto en buscar!!'
-        })
-    } else {
-        limpiarProductoshtml();
-        TraerProductoMercadoLibre(texto).then( () => { 
-            armoElCarrito();
-        })
-    }
-}
-function updateValueTablaLocal(){
-    limpiarProductoshtml();
-    traerProductosEnJson().then( () => { 
-        const imputId = document.getElementById("fname");
-        const texto = imputId.value.toUpperCase();
-        const resultado= (texto != "") ? buscarProductos(texto):[];
-        for (const producto of resultado) {
-            agregaProductohtml(producto);
-        }        
-    })
-}
+
 const ordenPrecio=document.getElementById("OrdenPrecio")
 ordenPrecio.addEventListener('click',ordenarPrecio)
 
